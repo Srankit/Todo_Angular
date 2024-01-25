d1cc842832a0ea3452e1dce7d283f517df0c9b21
Ankit Dubey <90557104+Srankit@users.noreply.github.com>
2024-01-25 21:19:15 +0530

    intial commit

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,7 +1,17 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
+import { LoginComponent } from './list/login/login.component';
+import { WelcomeComponent } from './list/welcome/welcome.component';
+import { ErrorComponent } from './list/error/error.component';
+import { ListTodosComponent } from './list/list-todos/list-todos.component';
 
-const routes: Routes = [];
+const routes: Routes = [
+  //{ path: '/', component: LoginComponent },
+  { path: 'login', component: LoginComponent },
+  { path: 'welcome/:name', component: WelcomeComponent },
+  { path: 'todo', component: ListTodosComponent },
+  { path: '**', component: ErrorComponent }
+];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,16 +1,25 @@
 import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
-
+import { FormsModule } from '@angular/forms';
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { WelcomeComponent } from './list/welcome/welcome.component';
+import { LoginComponent } from './list/login/login.component';
+import { ErrorComponent } from './list/error/error.component';
+import { ListTodosComponent } from './list/list-todos/list-todos.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    WelcomeComponent,
+    LoginComponent,
+    ErrorComponent,
+    ListTodosComponent
   ],
   imports: [
     BrowserModule,
-    AppRoutingModule
+    AppRoutingModule,
+    FormsModule
   ],
   providers: [],
   bootstrap: [AppComponent]
